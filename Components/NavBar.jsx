79b7eddca51154b024312b71d4cc16cb01423876
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-10-28 23:10:18 -0700

    Styled the nav bar

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/travel-blog-logo.png";
+
 const NavBar = () => {
   return (
-    <nav>Nav Bar</nav>
-  )
-}
+    <nav className="nav-container">
+      <div className="nav-item-container">
+        <Link href="/">
+          <Image src={logo} alt="Travel Blog Logo" width={140} height={140} />
+        </Link>
+      </div>
+      <div className="nav-item-container">
+        <p>xxxx</p>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
