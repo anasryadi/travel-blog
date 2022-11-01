6e4cc8d619e66ca7755dfbecdb29cf0dd2d3cbfe
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-11-01 15:03:15 -0700

    designed the server side rendering

---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,44 @@
+import groq from "groq";
+import Tag from "../../Components/Tag";
+import { PortableText } from "@portabletext/react";
+import { urlFor } from "../../lib/sanity";
+import { getClient } from "../../lib/sanity.server";
+
+const Post = ({ post }) => {
+  console.log(post);
+
+  return <div></div>;
+};
+const query = groq`*[_type == "post" && slug.current == $slug][0] {
+    title,
+    "username": author->username,
+    "about": author->bio,
+    "categories": categories[]->{id, title},
+    "authorImage": author->avatar,
+    body,
+    publishedAt,
+    mainImage,
+    postedAt
+}`;
+
+export async function getStaticPaths() {
+  const paths = await getClient().fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  );
+  return {
+    paths: paths.map((slug) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params, preview = false }) {
+  const post = await getClient(preview).fetch(query, { slug: params.slug });
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export default Post;
